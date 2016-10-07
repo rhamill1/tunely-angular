b6625b5fada1dce8b522c922bd2182ce3ebc971b
rhamill1 <rhamill11@gmail.com>
2016-10-07 10:03:36 -0700

    sprint 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,14 +18,18 @@ function AlbumsIndexController ($http) {
     artistName: 'Morrissey'
   };
 
-  $http({
-    method: 'GET',
-    url: '/api/albums'
-  }).then(function successCallback(response) {
-    vm.albums = response.data;
-  }, function errorCallback(response) {
-    console.log('There was an error getting the data', response);
-  });
+  vm.getAll = function() {
+    $http({
+      method: 'GET',
+      url: '/api/albums'
+    }).then(function successCallback(response) {
+      vm.albums = response.data;
+    }, function errorCallback(response) {
+      console.log('There was an error getting the data', response);
+    });
+  }
+
+  vm.getAll();
 
   vm.createAlbum = function () {
     $http({
@@ -38,4 +42,33 @@ function AlbumsIndexController ($http) {
       console.log('There was an error posting the data', response);
     });
   }
+
+  vm.deleteAlbum = function (album) {
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/' + album._id,
+    }).then(function successCallback(response) {
+      var pos = vm.albums.map(function(arrayAlbum) { return arrayAlbum._id}).indexOf(album._id);
+      vm.albums.splice(pos, 1);
+      console.log(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
+
+  vm.editAlbum = function (album) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/' + album._id,
+      data: {
+        artistName: vm.albums.artistName,
+        name: vm.albums.name
+      }
+    }).then(function successCallback(response) {
+      vm.getAll();
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
+
 }
