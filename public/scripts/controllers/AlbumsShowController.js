4888ab41059118e17e2085c6ccdd6428df3ea9d7
rhamill1 <rhamill11@gmail.com>
2016-10-07 13:46:27 -0700

    almost working push

---
+++ public/scripts/controllers/AlbumsShowController.js
@@ -0,0 +1,16 @@
+angular
+  .module('tunely')
+  .controller('AlbumsShowController', AlbumsShowController);
+
+AlbumsShowController.$inject = ['$http', '$routeParams'];
+function AlbumsShowController (  $http,   $routeParams  ) {
+  var vm = this;
+  console.log($routeParams);
+
+  $http({
+    method: 'GET',
+    url: '/api/albums/'+$routeParams.id
+  }).then(function successCallback(json) {
+    vm.album = json.data;
+  });
+}
